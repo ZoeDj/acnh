94e031a47c5b6cf5ae318ba414edc5e395467ff6
ZoeDj <studio3fun@gmail.com>
2020-07-16 17:02:11 -0400

    characteristics

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -7,6 +7,7 @@ class Discover extends Component {
     villager: "",
     image: "",
     name: "",
+    song: "",
   };
 
   componentDidMount() {
@@ -14,11 +15,17 @@ class Discover extends Component {
   }
 
   randomVillager = () => {
-    API.getRandomVillager()
+    let random = [Math.floor(Math.random() * 390)];
+    API.getRandomVillager(random)
+
       .then((res) =>
         this.setState({
-          image: Object.values(res.data)[Math.floor(Math.random() * 390)]
-            .image_uri,
+          name: Object.values(res.data)[random].name[`name-USen`],
+          image: Object.values(res.data)[random].image_uri,
+          birthday: Object.values(res.data)[random].birthday,
+          species: Object.values(res.data)[random].species,
+          gender: Object.values(res.data)[random].gender,
+          personality: Object.values(res.data)[random].personality,
         })
       )
       .catch((err) => console.log(err));
@@ -30,6 +37,11 @@ class Discover extends Component {
         <h1>Make New Friends</h1>
         <h3>Thumbs up on any villager you'd like to meet!</h3>
         <img src={this.state.image} alt="villager" />
+        <h4>Name: {this.state.name}</h4>
+        <h4>Gender: {this.state.gender}</h4>
+        <h4>Species: {this.state.species}</h4>
+        <h4>Birthday: {this.state.birthday}</h4>
+        <h4>Personality: {this.state.personality}</h4>
 
         <div>
           <button className="btn-up">
