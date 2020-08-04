d4a66c985416b95964a976f9b3a86af97c269979
ZoeDj <studio3fun@gmail.com>
2020-08-04 15:23:41 -0400

    Updates

---
+++ precache-manifest.4b8ca0b43b9246c1dc451e34790e49c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63ae43583c45bea3baf2db5f6f28e95",
+    "url": "/acnh/index.html"
+  },
+  {
+    "revision": "f7a6a3bfaafde0fee486",
+    "url": "/acnh/static/css/main.f27feb52.chunk.css"
+  },
+  {
+    "revision": "389ef7cc6f8af3d5915a",
+    "url": "/acnh/static/js/2.caaa5850.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/acnh/static/js/2.caaa5850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a6a3bfaafde0fee486",
+    "url": "/acnh/static/js/main.fe2beb5e.chunk.js"
+  },
+  {
+    "revision": "68690f6eb4116d890f0d",
+    "url": "/acnh/static/js/runtime-main.bc0bcdcb.js"
+  },
+  {
+    "revision": "35f6321dc7c739acf2a8561e48f4be04",
+    "url": "/acnh/static/media/img.35f6321d.png"
+  }
+]);
